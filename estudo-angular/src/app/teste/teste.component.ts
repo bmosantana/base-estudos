2e941a16c3dfc65b93964235fb27f184203ffbba
Brenda Santana <santanabrenda474@gmail.com>
2024-12-27 01:53:42 -0300

    feat: incluindo um novo componente

---
+++ estudo-angular/src/app/teste/teste.component.ts
@@ -0,0 +1,11 @@
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+@Component({
+  selector: 'app-teste', //nome da tag html para representar
+  imports: [], //import de novos modulos terceiros
+  templateUrl: './teste.component.html', //caminho do html da tela
+  styleUrl: './teste.component.scss', //caminho do arquivo de stilização
+})
+export class TesteComponent { //classe do componente que será exportada
+
+}
